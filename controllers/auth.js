e7550ef38e713ff1cc928754138181ab57c0faeb
zahirekrem09 <1820980144@stu.adu.edu.tr>
2020-12-05 15:40:36 +0200

    Add user register

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,22 @@
-const register = (req, res, next) => {
+const User = require("../models/User");
+
+const register = async (req, res, next) => {
+  //post Data
+  const name = "Ekrem";
+  const email = "ekrem@gmail.com";
+  const password = "123456";
+
+  // async await;
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+
   res.status(200).json({
     success: true,
+    data: user,
   });
 };
 
