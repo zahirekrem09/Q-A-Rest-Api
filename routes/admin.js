bc1afc264bc585e89a224e6c3b9f4f394d477ea6
zahirekrem09 <1820980144@stu.adu.edu.tr>
2020-12-07 18:01:30 +0200

    Add user Blocked

--- routes/admin.js
+++ routes/admin.js
@@ -1,15 +1,19 @@
 const express = require("express");
 const getAccessToRoute = require("../middlewares/auth/getAccessToRoute");
 const getAdminAccess = require("../middlewares/auth/getAdminAccess");
+const { blockUser } = require("../controllers/admin");
+const checkUserExist = require("../middlewares/db/checkUserExist");
 const router = express.Router();
 
 // /api/admin
 router.use([getAccessToRoute, getAdminAccess]);
-router.get("/", (req, res, next) => {
-  res.status(200).json({
-    success: true,
-    message: "Admin Page",
-  });
-});
+
+router.get("/block/:id", checkUserExist, blockUser);
+// router.get("/", (req, res, next) => {
+//   res.status(200).json({
+//     success: true,
+//     message: "Admin Page",
+//   });
+// });
 
 module.exports = router;
