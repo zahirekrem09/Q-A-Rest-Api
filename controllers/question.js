8f4be75997d734dbf0c602c1398ad4ccfa3b0669
zahirekrem09 <1820980144@stu.adu.edu.tr>
2020-12-08 12:53:04 +0200

    Add sinlge question add

--- controllers/question.js
+++ controllers/question.js
@@ -1,9 +1,21 @@
-const getAllQuestions = (req, res, next) => {
+const Question = require("../models/Question");
+const asyncErrorWrapper = require("express-async-handler");
+const CustomError = require("../helpers/error/CustomError");
+
+const askNewQuestions = asyncErrorWrapper(async (req, res, next) => {
+  const information = req.body;
+
+  const question = await Question.create({
+    ...information,
+    user: req.user.id,
+  });
+
   res.status(200).json({
     success: true,
+    data: question,
   });
-};
+});
 
 module.exports = {
-  getAllQuestions,
+  askNewQuestions,
 };
--- routes/question.js
+++ routes/question.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const getAccessToRoute = require("../middlewares/auth/getAccessToRoute");
 
-const { getAllQuestions } = require("../controllers/question");
+const { askNewQuestions } = require("../controllers/question");
 
 const router = express.Router();
 // /api/question
-router.get("/", getAllQuestions);
+// router.get("/", getAllQuestions);
+router.post("/ask", getAccessToRoute, askNewQuestions);
 
 module.exports = router;
